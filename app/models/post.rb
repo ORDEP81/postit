19849a7edc6b9f7c11bbb56dc586e7d5d06372e6
Ordep81 <Ordep81@gmail.com>
2013-11-02 17:21:09 -0400

    final push

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,29 +1,17 @@
 class Post < ActiveRecord::Base
+  
+  include Voteable
+
   belongs_to :user
   has_many :comments
   has_many :post_categories
   has_many :categories, through: :post_categories
-  has_many :votes, as: :voteable
+  
 
   before_save :generate_slug
 
   validates :title, presence: true
   validates :url, presence: true
-
-  
-  
-
-  def total_votes
-   self.up_votes - self.down_votes
-  end
-
-  def up_votes
-    self.votes.where(vote: true).size
-  end
-
-  def down_votes
-    self.votes.where(vote: false).size
-  end
 
   def generate_slug
     self.slug = self.title.gsub(' ', '-').downcase
